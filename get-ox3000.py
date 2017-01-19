522e1f82c6b2c9df022098b7d5837a4c16d8f3f6
Peng Guanwen <pgw1999@outlook.com>
2017-01-19 10:07:20 +0800

    BUG fix.
    - Set max retries.
    - Handle words with only one definition.
    - Handle definition with html tag.
    - Add linebreak.

--- get-ox3000.py
+++ get-ox3000.py
@@ -3,6 +3,11 @@
 from lxml import html, etree
 import requests, sys
 
+from requests.adapters import HTTPAdapter
+
+session = requests.Session()
+session.mount('http://', HTTPAdapter(max_retries=10))
+
 def get_entries():
     """Get all the Oxford 3000 (R) wrods from http://www.oxfordlearnersdictionaries.com/wordlist/english/oxford3000/Oxford3000 and links to their definition."""
     # make links of every page groups
@@ -18,7 +23,7 @@ def get_entries():
         while len(next_link) > 0:
             # download it
             print("downloading page from %s ..." % next_link[0], file=sys.stderr)
-            content = html.fromstring(requests.get(next_link[0]).text)
+            content = html.fromstring(session.get(next_link[0]).text)
 
             # get all the words in this page
             entries_url = content.cssselect('[title~=definition]')
@@ -37,14 +42,20 @@ def innerHTML(sentence):
 
 def get_definitions(link):
     print("downloading page from %s ..." % link, file=sys.stderr)
-    content = html.fromstring(requests.get(link).text)
-    ox3000defs = content.cssselect('.sn-gs > [ox3000=y]')
+    content = html.fromstring(session.get(link).text)
+    ox3000defs = content.cssselect('.sn-gs > .sn-g')
+    if len(ox3000defs) > 1:
+        ox3000defs = content.cssselect('.sn-gs > [ox3000=y]')
+
     defhtml = '<ol> '
     for defitem in ox3000defs:
         # get definition of this item
         defelems = defitem.cssselect('.def')
         if len(defelems) > 1: print('Def from', link, 'has multi .def element', file=sys.stderr)
-        definition = defelems[0].text
+        if len(defelems) > 0:
+            definition = defelems[0].text_content()
+        else:
+            definition = ''
 
         examples = defitem.cssselect('.sn-g > .x-gs > .x-g .x')
         examhtml = '<ul class = "eg"> '
@@ -63,7 +74,7 @@ def main():
     entries = get_entries()
     for (word, link) in entries:
         definitions = get_definitions(link)
-        print(word, link, definitions, sep='\t', end='')
+        print(word, link, definitions, sep='\t')
         
 
 if __name__ == '__main__':
