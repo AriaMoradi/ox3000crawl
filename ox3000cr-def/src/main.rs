1115e896308b8b78cb4125675641ba898c992b95
Peng Guanwen <pgw1999@outlook.com>
2017-02-25 14:54:34 +0800

    Migration: initial work
    - Rewrite .gitignore
    - Add dependencies
    - Initial functions

--- ox3000cr-def/src/main.rs
+++ ox3000cr-def/src/main.rs
@@ -1,3 +1,16 @@
+#![feature(conservative_impl_trait)]
+#![allow(dead_code)]
+extern crate scraper;
+
+use scraper::Html;
+
+const entry_groups_url_prefix: &str = "http://www.oxfordlearnersdictionaries.com/wordlist/english/oxford3000/Oxford3000_";
+const entry_groups: &[&str] = &["A-B", "C-D", "E-G", "H-K", "L-N", "O-P", "Q-R", "S", "T", "U-Z"];
+
+fn get_entries() -> impl Iterator<Item=(String, String, Html)> {
+    vec![].into_iter()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let entries = get_entries();
 }
