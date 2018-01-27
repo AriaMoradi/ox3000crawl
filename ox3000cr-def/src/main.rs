014c7d4170e953521dc19b82ab9fed1c5f1eb406
Peng Guanwen <pg999w@outook.com>
2018-01-27 04:56:32 +0000

    Update the rust project
    Make web request works.

--- ox3000cr-def/src/main.rs
+++ ox3000cr-def/src/main.rs
@@ -1,16 +1,49 @@
 #![feature(conservative_impl_trait)]
-#![allow(dead_code)]
-extern crate scraper;
+#![feature(try_trait)]
+#![feature(use_nested_groups)]
 
-use scraper::Html;
+extern crate futures;
+extern crate hyper;
+extern crate kuchiki;
+extern crate tokio_core;
 
-const entry_groups_url_prefix: &str = "http://www.oxfordlearnersdictionaries.com/wordlist/english/oxford3000/Oxford3000_";
-const entry_groups: &[&str] = &["A-B", "C-D", "E-G", "H-K", "L-N", "O-P", "Q-R", "S", "T", "U-Z"];
+// use std::io::{self, Write};
+use futures::{Future, Stream, stream::futures_unordered};
+use hyper::Client;
+use tokio_core::reactor::Core;
+use kuchiki::parse_html;
 
-fn get_entries() -> impl Iterator<Item=(String, String, Html)> {
-    vec![].into_iter()
+const ENTRY_GROUPS_URL_PREFIX: &str =
+    "http://www.oxfordlearnersdictionaries.com/wordlist/english/oxford3000/Oxford3000_";
+const ENTRY_GROUPS: &[&str] = &[
+    "A-B", "C-D", "E-G", "H-K", "L-N", "O-P", "Q-R", "S", "T", "U-Z"
+];
+
+// fn entrie_uris_from_body(body: Html) -> impl Iterator {}
+
+fn entry_uris() {
+    let mut core = Core::new().unwrap();
+    let client = Client::new(&core.handle());
+
+    let uris = ENTRY_GROUPS
+        .iter()
+        .map(|suffix| ENTRY_GROUPS_URL_PREFIX.to_string() + suffix)
+        .map(|uri| uri.parse().unwrap());
+    let work = uris.map(|uri| {
+        client.get(uri).and_then(|res| {
+            println!("Response: {}", res.status());
+            res.body()
+                .concat2()
+                .and_then(move |body| parse_html().from_utf8().from_iter(body))
+        })
+    });
+    let list_stream = futures_unordered(work);
+
+    let res = core.run(list_stream.collect());
+    println!("{:?}", res)
 }
 
 fn main() {
-    let entries = get_entries();
+    entry_uris()
+    // let entries = get_entries();
 }
