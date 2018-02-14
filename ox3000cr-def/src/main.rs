57959c3bb1a85f377623dc4669f964728cc2af24
Peng Guanwen <pg999w@outook.com>
2018-02-14 10:34:14 +0000

    Try to use stream::unfold

--- ox3000cr-def/src/main.rs
+++ ox3000cr-def/src/main.rs
@@ -10,7 +10,7 @@ extern crate tokio_core;
 
 use futures::{stream, Future, Stream};
 use hyper::{Client, Uri};
-use tokio_core::reactor::{Core,Handle};
+use tokio_core::reactor::{Core, Handle};
 use scraper::{Html, Selector};
 
 const ENTRY_GROUPS_URL_PREFIX: &str =
@@ -19,11 +19,19 @@ const ENTRY_GROUPS: &[&str] = &[
     "A-B", "C-D", "E-G", "H-K", "L-N", "O-P", "Q-R", "S", "T", "U-Z"
 ];
 
-fn entry_uris_from_body(body: &Html) -> Vec<String> {
+fn entry_uris_from_body(body: &Html) -> (Vec<String>, Option<Uri>) {
     let select_word = Selector::parse("[title~=definition]").unwrap();
-    body.select(&select_word)
+    let uris = body.select(&select_word)
         .map(|m| m.value().attr("href").unwrap().to_string())
-        .collect()
+        .collect();
+
+    let select_next = Selector::parse("#paging a").unwrap();
+    let link = body.select(&select_next)
+        .filter(|m| m.text().next().unwrap() == ">")
+        .next()
+        .map(|m| m.value().attr("href").unwrap().parse().unwrap());
+
+    (uris, link)
 }
 
 fn entry_uris(handle: &Handle) -> impl Stream<Item = String, Error = hyper::Error> {
@@ -32,24 +40,48 @@ fn entry_uris(handle: &Handle) -> impl Stream<Item = String, Error = hyper::Erro
     let uris = ENTRY_GROUPS
         .iter()
         .map(|suffix| ENTRY_GROUPS_URL_PREFIX.to_string() + suffix)
-        .map(|uri| uri.parse().unwrap());
+        .map(|uri| uri.parse::<Uri>().unwrap());
 
-    stream::iter_ok(uris)
-        .map(move |uri: Uri| {
-            println!("get: {}", uri);
-            client.get( uri.clone() ).map(move |res| {
-                eprintln!("Downloaded page from {}\nResponse: {}", uri, res.status());
-                res
+    let jobs = uris.map(|uri| {
+        stream::unfold(Some(uri), |uri| {
+            uri.map(|uri| {
+                eprintln!("get: {}", uri);
+                client
+                    .get(uri.clone())
+                    .map(move |res| {
+                        eprintln!("Downloaded page from {}\nResponse: {}", uri, res.status());
+                        res
+                    })
+                    .and_then(|res| res.body().concat2())
+                    .map(|body| {
+                        let doc = Html::parse_document(&*String::from_utf8_lossy(&*body));
+                        entry_uris_from_body(&doc)
+                    })
             })
-        })
-        .buffer_unordered(10)
-        .and_then(|res| res.body().concat2())
-        .map(|body| {
-            let doc = Html::parse_document(&*String::from_utf8_lossy(&*body));
-            entry_uris_from_body(&doc)
-        })
+        }).into_future()
+    });
+
+    stream::futures_unordered(jobs)
+        .map(|(u, s)| stream::once(Ok(u.unwrap())).chain(s))
+        .flatten()
         .map(stream::iter_ok)
         .flatten()
+    // stream::iter_ok(uris)
+    //     .map(move |uri: Uri| {
+    //         println!("get: {}", uri);
+    //         client.get(uri.clone()).map(move |res| {
+    //             eprintln!("Downloaded page from {}\nResponse: {}", uri, res.status());
+    //             res
+    //         })
+    //     })
+    //     .buffer_unordered(10)
+    //     .and_then(|res| res.body().concat2())
+    //     .map(|body| {
+    //         let doc = Html::parse_document(&*String::from_utf8_lossy(&*body));
+    //         entry_uris_from_body(&doc)
+    //     })
+    //     .map(stream::iter_ok)
+    //     .flatten()
 }
 
 fn main() {
