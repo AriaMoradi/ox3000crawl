0d6b2536a25c0b715c08f0dfa34674d24c76f743
Peng Guanwen <pg999w@outook.com>
2018-02-15 15:12:41 +0000

    Update doc

--- ox3000cr-def/doc/implementors/alloc/string/trait.ToString.js
+++ ox3000cr-def/doc/implementors/alloc/string/trait.ToString.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors["kuchiki"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"struct\" href=\"kuchiki/struct.NodeRef.html\" title=\"struct kuchiki::NodeRef\">NodeRef</a>",];
+implementors["openssl"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"enum\" href=\"openssl/x509/extension/enum.Extension.html\" title=\"enum openssl::x509::extension::Extension\">Extension</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
